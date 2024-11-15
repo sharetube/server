e90e68124e49be84f003048e84257cc6105be139
skewb1k <101928759+skewb1k@users.noreply.github.com>
2024-11-15 08:49:17 +0300

    refactor

--- internal/handler.go
+++ internal/handler.go
@@ -1,108 +1,64 @@
 package internal
 
 import (
-	"fmt"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 )
 
 type Handler struct {
-	rooms    map[string]*Room
-	upgrader websocket.Upgrader
+	upgrader    websocket.Upgrader
+	roomService RoomService
 }
 
 func NewHandler() *Handler {
 	return &Handler{
-		rooms: make(map[string]*Room),
 		upgrader: websocket.Upgrader{
 			CheckOrigin: func(r *http.Request) bool {
 				return true
 			},
 		},
+		roomService: NewRoomService(),
 	}
 }
 
-func (h Handler) JoinToRoomAndListen(w http.ResponseWriter, r *http.Request, room *Room, userID, username, color string) {
-	headers := http.Header{}
-	// headers.Add("Set-Cookie", cookieString)
-	conn, err := h.upgrader.Upgrade(w, r, headers)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer conn.Close()
-	fmt.Println("conn upgraded")
+// func (h Handler) ReadMessages(conn *websocket.Conn, hub *hub, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	for {
+// 		var input Input
+// 		err := conn.ReadJSON(&input)
+// 		if err != nil {
+// 			fmt.Println("ReadJson error", err)
+// 			hub.RemoveMemberByConn(conn)
+// 			return
+// 		}
+// 		input.Sender = conn
 
-	member := Member{
-		ID:       userID,
-		Username: username,
-		Color:    color,
-		Conn:     conn,
-	}
-	room.AddMember(&member)
+// 		fmt.Printf("Message recieved: %v\n", input)
+// 		hub.ReadMessage(conn, input)
+// 	}
+// }
 
-	for {
-		var msg Message
-		err := conn.ReadJSON(&msg)
-		if err != nil {
-			fmt.Println("ReadJson error", err)
-			if err := room.RemoveMemberByConn(conn); err != nil {
-				fmt.Printf("remove member by conn error: %s\n", err)
-				return
-			}
-			return
-		}
+// func (h Handler) JoinRoomAndListen(w http.ResponseWriter, r *http.Request, hub *hub, member *models.Member) {
+// 	headers := http.Header{}
+// 	// headers.Add("Set-Cookie", cookieString)
+// 	conn, err := h.upgrader.Upgrade(w, r, headers)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	fmt.Println("conn upgraded")
 
-		fmt.Printf("Message recieved: %v\n", msg)
-		room.Broadcast <- msg
-	}
-}
+// 	member.Conn = conn
+// 	hub.AddMember(conn, member)
 
-func (h Handler) Mux() http.Handler {
-	r := chi.NewRouter()
+// 	wg := sync.WaitGroup{}
+// 	wg.Add(1)
+// 	go h.ReadMessages(conn, hub, &wg)
+// 	wg.Wait()
 
-	r.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		userID := uuid.NewString()
-		fmt.Printf("/ws userID: %s\n", userID)
+// 	hub.SendStateToMemberByConn(conn)
 
-		roomID := uuid.NewString()
-		fmt.Printf("/ws roomID: %s\n", roomID)
-
-		room := NewRoom(roomID, userID)
-		h.rooms[roomID] = room
-		fmt.Printf("/ws room created: %#v\n", room)
-
-		go room.HandleMessages()
-
-		h.JoinToRoomAndListen(w, r, room, userID, "some-username", "some-color")
-	})
-
-	r.HandleFunc("/ws/{room-id}", func(w http.ResponseWriter, r *http.Request) {
-		// for _, c := range r.Cookies() {
-		// 	fmt.Printf("Cookie: %#v\n", c)
-		// }
-
-		roomID := chi.URLParam(r, "room-id")
-		if roomID == "" {
-			fmt.Fprint(w, "room-id was not provided")
-			return
-		}
-
-		userID := uuid.NewString()
-		fmt.Printf("/ws/room userID: %s\n", userID)
-
-		room := h.rooms[roomID]
-		fmt.Printf("/ws/room roomID: %s\n", roomID)
-		if room == nil {
-			fmt.Fprint(w, "room not found")
-			return
-		}
-
-		h.JoinToRoomAndListen(w, r, room, userID, "joined-username", "joined-color")
-	})
-
-	return r
-}
+// 	fmt.Println("closing connection")
+// 	conn.Close()
+// }
