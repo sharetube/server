d0f6d571cabff87ef7b16f18e93749ee0b016548
skewb1k <101928759+skewb1k@users.noreply.github.com>
2024-11-16 19:05:58 +0300

    sender

--- internal/controller/helper.go
+++ internal/controller/helper.go
@@ -5,35 +5,15 @@ import (
 	"net/http"
 )
 
-func GetUsername(w http.ResponseWriter, r *http.Request) string {
-	username := r.URL.Query().Get("username")
-	if username == "" {
-		fmt.Println("username was not provided")
-		fmt.Fprint(w, "username was not provided")
-		return ""
+const (
+	headerPrefix = "St-"
+)
+
+func MustHeader(r *http.Request, key string) (string, error) {
+	value := r.Header.Get(headerPrefix + key)
+	if value == "" {
+		return "", fmt.Errorf("%s was not provided", key)
 	}
 
-	return username
-}
-
-func GetColor(w http.ResponseWriter, r *http.Request) string {
-	color := r.URL.Query().Get("color")
-	if color == "" {
-		fmt.Println("color was not provided")
-		fmt.Fprint(w, "color was not provided")
-		return ""
-	}
-
-	return color
-}
-
-func GetVideoURL(w http.ResponseWriter, r *http.Request) string {
-	videoURL := r.URL.Query().Get("video-url")
-	if videoURL == "" {
-		fmt.Println("video url was not provided")
-		fmt.Fprint(w, "video-url was not provided")
-		return ""
-	}
-
-	return videoURL
+	return value, nil
 }
--- internal/domain/room.go
+++ internal/domain/room.go
@@ -3,7 +3,6 @@ package domain
 import (
 	"fmt"
 	"strconv"
-	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -47,14 +46,19 @@ func (r Room) GetState() map[string]any {
 	}
 }
 
-func (r *Room) AddMember(member *Member) error {
-	return r.members.Add(member)
+func (r *Room) AddMember(member *Member) {
+	if err := r.members.Add(member); err != nil {
+		r.SendError(member.Conn, err)
+		return
+	}
+
+	r.SendMemberJoined(member)
 }
 
 func (r *Room) RemoveMemberByID(id string) {
 	member, err := r.members.RemoveByID(id)
 	if err != nil {
-		fmt.Printf("remove member by conn: %s\n", err)
+		fmt.Printf("remove member by id: %s\n", err)
 		return
 	}
 
@@ -64,14 +68,7 @@ func (r *Room) RemoveMemberByID(id string) {
 		return
 	}
 
-	r.SendMessageToAllMembers(&Message{
-		Type: "member_left",
-		Data: map[string]any{
-			"member":        member,
-			"members":       r.members.AsList(),
-			"members_count": r.members.Length(),
-		},
-	})
+	r.SendMemberLeft(&member)
 }
 
 func (r *Room) RemoveMemberByConn(conn *websocket.Conn) {
@@ -87,14 +84,7 @@ func (r *Room) RemoveMemberByConn(conn *websocket.Conn) {
 		return
 	}
 
-	r.SendMessageToAllMembers(&Message{
-		Type: "member_left",
-		Data: map[string]any{
-			"member":        member,
-			"members":       r.members.AsList(),
-			"members_count": r.members.Length(),
-		},
-	})
+	r.SendMemberLeft(&member)
 }
 
 func (r *Room) AddVideo(addedBy *websocket.Conn, url string) (Video, error) {
@@ -108,15 +98,6 @@ func (r *Room) AddVideo(addedBy *websocket.Conn, url string) (Video, error) {
 
 func (r *Room) RemoveVideo(videoIndex int) (Video, error) {
 	return r.playlist.RemoveByID(videoIndex)
-}
-
-func (r *Room) SendError(conn *websocket.Conn, err error) {
-	r.SendMessageToConn(conn, &Message{
-		Type: "error",
-		Data: map[string]any{
-			"message": err,
-		},
-	})
 }
 
 func (r *Room) ReadMessages(conn *websocket.Conn) {
@@ -142,59 +123,34 @@ func (r *Room) HandleMessages(input Input) {
 			return
 		case input := <-r.inputCh:
 			fmt.Printf("message recieved: %#v\n", input)
-			var outMsg Message
 			switch input.Action {
 			case "get_state":
-				outMsg = Message{
+				r.SendMessageToAllMembers(&Message{
 					Type: "state",
 					Data: r.GetState(),
-				}
+				})
 			case "add_video":
-				r.AddVideo(nil, *input.Data)
-			case "remove_video":
-				videoIndex, err := strconv.Atoi(*input.Data)
+				video, err := r.AddVideo(input.Sender, *input.Data)
 				if err != nil {
-					fmt.Printf("remove %s\n", err)
+					fmt.Printf("add video %s\n", err)
 					r.SendError(input.Sender, err)
 				}
 
-				r.RemoveVideo(videoIndex)
+				r.SendVideoAdded(&video)
+			case "remove_video":
+				videoIndex, err := strconv.Atoi(*input.Data)
+				if err != nil {
+					fmt.Printf("remove video error %s\n", err)
+					r.SendError(input.Sender, err)
+				}
+
+				video, err := r.RemoveVideo(videoIndex)
+				if err != nil {
+					r.SendError(input.Sender, err)
+				}
+
+				r.SendVideoRemoved(&video)
 			}
-
-			fmt.Printf("message to send: %#v\n", outMsg)
-			r.SendMessageToAllMembers(&outMsg)
-		}
-	}
-}
-
-func (r *Room) SendMessageToAllMembers(msg *Message) {
-	fmt.Println("sending message to all members")
-	for _, member := range r.members.AsList() {
-		r.SendMessageToConn(member.Conn, msg)
-	}
-}
-
-func (r *Room) SendMessageToConn(conn *websocket.Conn, msg *Message) {
-	fmt.Println("sending message to member")
-	if err := conn.WriteJSON(msg); err != nil {
-		fmt.Println(err)
-		conn.Close()
-		r.RemoveMemberByConn(conn)
-	}
-}
-
-func (r *Room) SendStateToAllMembersPeriodically(timeout time.Duration) {
-	for {
-		select {
-		case <-r.closeCh:
-			fmt.Println("stop spam")
-			return
-		default:
-			time.Sleep(timeout)
-			r.SendMessageToAllMembers(&Message{
-				Type: "update",
-				Data: r.GetState(),
-			})
 		}
 	}
 }
