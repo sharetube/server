91adaa291e8395bec618410ffd0c07be19ec3994
skewb1k <101928759+skewb1k@users.noreply.github.com>
2024-11-19 02:56:16 +0300

    redis related things

--- internal/controller/router.go
+++ internal/controller/router.go
@@ -10,8 +10,14 @@ func (c Controller) Mux() http.Handler {
 	r := chi.NewRouter()
 
 	r.Route("/api/v1", func(r chi.Router) {
-		r.HandleFunc("/ws/create-room", c.CreateRoom)
-		r.HandleFunc("/ws/join-room/{room-id}", c.JoinRoom)
+		r.Route("/room", func(r chi.Router) {
+			// r.Get("/{room-id}", c.GetRoom)
+			r.Route("/create", func(r chi.Router) {
+				r.Get("/validate", c.CreateRoom)
+				r.Get("/ws", c.CreateRoom)
+			})
+			r.Post("/join", c.JoinRoom)
+		})
 	})
 
 	return r
